92df65e09a685d97fcb99b71e92b41b89439357f
Denis <dsapaiv@gmail.com>
2020-11-16 16:37:51 +0200

    Added create_charge function

--- lib/stripe_client.ex
+++ lib/stripe_client.ex
@@ -37,20 +37,50 @@ defmodule StripeClient do
   end
 
   defp _balance_transactions(private_key, limit) when is_integer(limit) do
-    StripeClient.Utils.post("#{@base_url}/v1/balance_transactions?limit=#{limit}", [
+    StripeClient.Utils.get("#{@base_url}/v1/balance_transactions?limit=#{limit}", [
       {"Authorization", "Bearer #{private_key}"}
     ])
   end
 
   defp _balance_transactions(private_key, nil) do
-    StripeClient.Utils.get("#{@base_url}/v1/balance_transactions", [
-      {"Authorization", "Bearer #{private_key}"}
-    ])
+    StripeClient.Utils.get(
+      "#{@base_url}/v1/balance_transactions",
+      [{"Authorization", "Bearer #{private_key}"}]
+    )
   end
 
   def balance_transaction(private_key, transaction_id) when is_bitstring(transaction_id) do
     StripeClient.Utils.get("#{@base_url}/v1/balance_transactions/#{transaction_id}", [
       {"Authorization", "Bearer #{private_key}"}
     ])
+  end
+
+  @doc """
+    Creates charge with specified amount, currency, and description
+    You can view supported currencies at https://stripe.com/docs/currencies
+  """
+  def create_charge(
+        private_key,
+        amount,
+        description,
+        currency \\ "usd",
+        source \\ "tok_mastercard"
+      ) do
+    body =
+      URI.encode_query(%{
+        "amount" => amount,
+        "description" => description,
+        "currency" => currency,
+        "source" => source
+      })
+
+    StripeClient.Utils.post(
+      "#{@base_url}/v1/charges",
+      body,
+      [
+        {"Authorization", "Bearer #{private_key}"},
+        {"Content-Type", "application/x-www-form-urlencoded"}
+      ]
+    )
   end
 end
