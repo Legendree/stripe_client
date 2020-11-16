5f5f67cfeed18ac96bcddcc060364cbf91486e03
Denis <dsapaiv@gmail.com>
2020-11-16 18:57:07 +0200

    Added update_change() && retrive_change() added @spec for each of functions some functions already have basic @docs

--- lib/stripe_client.ex
+++ lib/stripe_client.ex
@@ -18,18 +18,29 @@ defmodule StripeClient do
     :world
   end
 
+  @doc """
+    Your API keys carry many privileges, so be sure to keep them secure!
+    Do not share your secret API keys in publicly accessible areas
+    such as GitHub, client-side code, and so forth.
+    Authentication to the API is performed via HTTP Basic Auth.
+    Provide your API key as the basic auth username value.
+    You do not need to provide a password.
+  """
+  @spec authenticate(String.t()) :: tuple()
   def authenticate(private_key) do
     StripeClient.Utils.get("#{@base_url}/v1/charges", [
       {"Authorization", "Bearer #{private_key}"}
     ])
   end
 
+  @spec balance(String.t()) :: tuple()
   def balance(private_key) do
     StripeClient.Utils.get("#{@base_url}/v1/balance", [
       {"Authorization", "Bearer #{private_key}"}
     ])
   end
 
+  @spec balance_transactions(String.t(), list()) :: tuple()
   def balance_transactions(private_key, opts \\ []) do
     limit = Keyword.get(opts, :limit)
 
@@ -49,6 +60,7 @@ defmodule StripeClient do
     )
   end
 
+  @spec balance_transaction(String.t(), String.t()) :: tuple()
   def balance_transaction(private_key, transaction_id) when is_bitstring(transaction_id) do
     StripeClient.Utils.get("#{@base_url}/v1/balance_transactions/#{transaction_id}", [
       {"Authorization", "Bearer #{private_key}"}
@@ -59,6 +71,7 @@ defmodule StripeClient do
     Creates charge with specified amount, currency, and description
     You can view supported currencies at https://stripe.com/docs/currencies
   """
+  @spec create_charge(String.t(), integer(), String.t(), String.t(), String.t()) :: tuple()
   def create_charge(
         private_key,
         amount,
@@ -82,5 +95,26 @@ defmodule StripeClient do
         {"Content-Type", "application/x-www-form-urlencoded"}
       ]
     )
+  end
+
+  @spec retrive_charge(String.t(), String.t()) :: tuple()
+  def retrive_charge(private_key, charge_id) do
+    StripeClient.Utils.get("#{@base_url}/v1/charges/#{charge_id}", [
+      {"Authorization", "Bearer #{private_key}"}
+    ])
+  end
+
+  @doc """
+    Updates desierd fields in any of provided charges id
+    parameters_to_update = %{"metadata[order_id]" => 6735}
+  """
+  @spec update_charge(String.t(), String.t(), map()) :: tuple()
+  def update_charge(private_key, charge_id, parameters_to_update \\ %{}) do
+    body = URI.encode_query(parameters_to_update)
+
+    StripeClient.Utils.post("#{@base_url}/v1/charges/#{charge_id}", body, [
+      {"Authorization", "Bearer #{private_key}"},
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ])
   end
 end
